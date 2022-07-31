fde079817b66c46863ac6b6ba4686f982369e2ac
alyson <consulialyson@gmail.com>
2022-07-31 12:55:10 -0300

    feat: first commit

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { json } from "express";
+import "express-async-errors";
+import cors from "cors";
+import "./config/setup.js";
+
+import router from "./routes/index.js";
+import { errorHandlingMiddleware } from "./middlewares/errorHandlingMiddleware.js";
+import { notFoundRouteHandlingMiddleware } from "./middlewares/notFoundRouteHandlingMiddleware.js";
+
+const app = express();
+app.use(cors());
+app.use(json());
+app.use(router);
+app.use(errorHandlingMiddleware);
+app.use(notFoundRouteHandlingMiddleware);
+
+export default app;
