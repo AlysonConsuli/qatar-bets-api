423da0ae9d0dc66ea4de9e7c75d26ad643a52d7b
alyson <consulialyson@gmail.com>
2022-08-04 02:55:17 -0300

    feat: router to auto-login

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -12,5 +12,9 @@ export const signup = async (req: Request, res: Response) => {
 export const signin = async (req: Request, res: Response) => {
   const user: UserInsertData = req.body;
   const token = await authService.signin(user);
-  res.send({ token });
+  res.send({ name: user.name, token });
+};
+
+export const autologin = async (req: Request, res: Response) => {
+  res.sendStatus(200);
 };
--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 
-import { signin, signup } from "../controllers/authController.js";
+import { autologin, signin, signup } from "../controllers/authController.js";
 import { validateSchema } from "../middlewares/schemaValidator.js";
+import { validateToken } from "../middlewares/validateToken.js";
 import { signinSchema, signupSchema } from "../schemas/authSchema.js";
 
 const authRouter = Router();
 
 authRouter.post("/sign-up", validateSchema(signupSchema), signup);
 authRouter.post("/sign-in", validateSchema(signinSchema), signin);
+authRouter.post("/auto-login", validateToken, autologin);
 
 export default authRouter;
