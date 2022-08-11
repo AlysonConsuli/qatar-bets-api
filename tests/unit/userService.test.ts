04bb4af2a5c828b11df2e37fafa070f783470839
alyson <consulialyson@gmail.com>
2022-08-11 15:46:43 -0300

    feat: signin tests

--- tests/unit/userService.test.ts
+++ tests/unit/userService.test.ts
@@ -4,11 +4,13 @@ import { authService } from "../../src/services/authService.js";
 import { authRepository } from "../../src/repositories/authRepository.js";
 import { appRepository } from "../../src/repositories/appRepository.js";
 import { userBody } from "../factories/userFactory.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-describe("authService test suite", () => {
-  const user = { ...userBody(), id: 1, isPaid: false };
-  const { id, name, password, isPaid } = user;
+const user = { ...userBody(), id: 1, isPaid: false, createdAt: null };
+const { id, name, password, isPaid } = user;
 
+describe("signup test suite", () => {
   it("should create user", async () => {
     jest.spyOn(authRepository, "findUserByName").mockResolvedValueOnce(null);
     jest.spyOn(appRepository, "insertData").mockResolvedValueOnce(null);
@@ -17,13 +19,40 @@ describe("authService test suite", () => {
   });
 
   it("given a user name equals to another, return conflict error", async () => {
-    jest
-      .spyOn(authRepository, "findUserByName")
-      .mockResolvedValueOnce(user as any);
+    jest.spyOn(authRepository, "findUserByName").mockResolvedValueOnce(user);
     const promise = authService.signup(user);
     expect(promise).rejects.toEqual({
       type: "conflict",
       message: "User already exists!",
+    });
+  });
+});
+
+describe("signin test suite", () => {
+  it("should signin and receive a token", async () => {
+    jest.spyOn(authRepository, "findUserByName").mockResolvedValueOnce(user);
+    jest.spyOn(bcrypt, "compareSync").mockReturnValueOnce(true);
+    jest.spyOn(jwt, "sign").mockReturnValueOnce("my_token");
+    const token = await authService.signin(user);
+    expect(token).toEqual("my_token");
+  });
+
+  it("given a user that doesnt exist, return not found error", async () => {
+    jest.spyOn(authRepository, "findUserByName").mockResolvedValueOnce(null);
+    const promise = authService.signin(user);
+    expect(promise).rejects.toEqual({
+      type: "notFound",
+      message: "User not found!",
+    });
+  });
+
+  it("given a incorrect password, return unauthorized error", async () => {
+    jest.spyOn(authRepository, "findUserByName").mockResolvedValueOnce(user);
+    jest.spyOn(bcrypt, "compareSync").mockReturnValueOnce(false);
+    const promise = authService.signin(user);
+    expect(promise).rejects.toEqual({
+      type: "unauthorized",
+      message: "Incorrect password!",
     });
   });
 });
