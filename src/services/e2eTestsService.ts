079964c06602ef62ee9e48689e0005721d307b5c
alyson <consulialyson@gmail.com>
2022-08-14 20:41:21 -0300

    fix: createGame Service

--- src/services/e2eTestsService.ts
+++ src/services/e2eTestsService.ts
@@ -2,7 +2,7 @@ import { e2eTestsRepository } from "../repositories/e2eTestsRepository.js";
 
 const createGame = async () => {
   const teams = [{ name: "Qatar" }, { name: "Ecuador" }];
-  const games = [{ team1Id: 2, team2Id: 2 }];
+  const games = [{ team1Id: 1, team2Id: 2 }];
   await e2eTestsRepository.createGame(teams, games);
 };
 
